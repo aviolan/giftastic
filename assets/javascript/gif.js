5eb6cbc83466bde9ef961282fe18745549fdbeca
Andrew Violan <andrew.violan@gmail.com>
2018-10-20 18:58:49 -0700

    initial site files

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,32 +1,7 @@
+var topics = ["The Office", "Parks and Rec", "It's Always Sunny In Philadelphia", "Impractical Jokers"];
+
 $(document).ready(function() {
-
-$("button").on("click", function() {
-    var television = $(this).attr("data-television");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        television + "&api_key=zkxPeruCYUg4IV4nKPU6kFuYTVX7vsmL&limit=10";
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-        .then(function(response) {
-            var results = response.data;
-
-            for (var i = 0; i < results.length; i++) {
-                var gifDiv = $("<div>");
-
-                var rating = results[i].rating;
-
-                var p = $("<p>").text("Rating: " + rating);
-
-                var televisionImage = $("<img>");
-                televisionImage.attr("src", results[i].images.fixed_height.url);
-
-                gifDiv.prepend(p);
-                gifDiv.prepend(televisionImage);
-
-                $("#gifs-appear-here").prepend(gifDiv);
-        }
-      });
-  });
+    for (var i = 0; i < topics.length; i++) {
+        $("#television-buttons").append("<button type='button' onclick='searchGif(\"" + topics[i] + "\")' class='btn btn-primary' value=' " + topics[i] + "'>" + topics[i] + "</button>");
+    }
 });
