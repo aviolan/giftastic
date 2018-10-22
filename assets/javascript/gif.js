9e1e548d3c7e32369ad24725cce697ee0604da97
Andrew Violan <andrew.violan@gmail.com>
2018-10-22 10:37:21 -0700

    initial site files

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -5,3 +5,17 @@ $(document).ready(function() {
         $("#television-buttons").append("<button type='button' onclick='searchGif(\"" + topics[i] + "\")' class='btn btn-primary' value=' " + topics[i] + "'>" + topics[i] + "</button>");
     }
 });
+
+function tvButton () {
+    var userInput = $("#television-input").val();
+    searchGif(userInput);
+}
+
+function submitButtonClicked () {
+    var userInput = $("#television-input").val();
+
+    if (userInput) {
+        $("#television-buttons").append("<button type='button' onclick='searchGif(\"" + userInput + "\")' class='btn btn-primary' value=' " + userInput + "'> " + userInput + " </button>");
+    }
+}
+
