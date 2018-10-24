bf2affe7bafa80d60c3bce08240a9dfac3154964
Andrew Violan <andrew.violan@gmail.com>
2018-10-23 18:08:31 -0700

    initial site files

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,16 +1,20 @@
-var topics = ["The Office", "Parks and Rec", "It's Always Sunny In Philadelphia", "Impractical Jokers"];
+//Topics array. I chose comedy shows
+var topics = ["The Office", "Parks and Rec", "Its Always Sunny In Philadelphia", "Impractical Jokers"];
 
+//Dynamically creates buttons onto html page
 $(document).ready(function() {
     for (var i = 0; i < topics.length; i++) {
         $("#television-buttons").append("<button type='button' onclick='searchGif(\"" + topics[i] + "\")' class='btn btn-primary' value=' " + topics[i] + "'>" + topics[i] + "</button>");
     }
 });
 
+//Creates tv button after user types in search box
 function tvButton () {
     var userInput = $("#television-input").val();
     searchGif(userInput);
 }
 
+//Appends the users input after they type in a tv show
 function submitButtonClicked () {
     var userInput = $("#television-input").val();
 
@@ -19,6 +23,7 @@ function submitButtonClicked () {
     }
 }
 
+//Searchs for gifs using the AJAX and GET method. Includes my API Key
 function searchGif(gifName) {
     $.ajax({
         url: "http://api.giphy.com/v1/gifs/search?q= " + gifName + " &api_key=zkxPeruCYUg4IV4nKPU6kFuYTVX7vsmL",
@@ -29,10 +34,28 @@ function searchGif(gifName) {
     })
 }
 
+//Appends the image of each tv show gif to the html page
 function displayGif(response) {
     $("#television").empty();
     for ( var i = 0; i < response.data.length; i++) {
         var rating = "<div class='ratings'> Rating: " + (response.data[i].rating) + "</div>";
+        var image = rating + '<img src= " ' + response.data[i].images.fixed_height_still.url +
+        ' " data-still=" ' + response.data[i].images.fixed_height_still.url +
+        ' " data-animate=" ' + response.data[i].images.fixed_height.url + ' " data-state="still" class="movImage" style="width: 300px; height: 300px">';
         
+        image = '<div class="col-md-4">' + image + "</div>";
+        $('#television').append(image);
     }
+
+    //Allows the user to click the gif to stop and play
+    $(".movImage").on("click", function () {
+        var state = $(this).attr("data-state");
+        if (state == "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+    });
 }
